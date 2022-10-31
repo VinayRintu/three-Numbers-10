5acd69393362fc366344d3f1c6896900e2b3051e
Rintu <Rintu@VINDUJA>
2022-10-31 16:51:39 +0530

    Add project files.

---
+++ three Numbers 10/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace three_Numbers_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter 1st nm");
+            int n1=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter 2nd nm");
+            int n2=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter 3rd");
+            int n3=Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Results of the numbers : {0},{1},{2},(x+y).z is{3}and x.y+y.z is{4}\n\n" ,n1,n2,n3,((n1+n2)*n3),(n1*n2+n2*n3));
+            Console.ReadLine();
+        }
+    }
+}
